75cc4a2d7e751fb434bc9a64587947591a10fba8
burturt <31748545+burturt@users.noreply.github.com>
2021-06-18 12:18:01 -0700

    update

--- bashcrawl/background.sh
+++ bashcrawl/background.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+apt install git less
+echo "done" >> /root/katacoda-finished
 git clone https://gitlab.com/slackermedia/bashcrawl.git
+echo "done" >> /root/katacoda-background-finished
--- bashcrawl/setup.sh
+++
@@ -1,3 +0,0 @@
-sudo apt install less git
-git clone https://gitlab.com/slackermedia/bashcrawl.git
-
